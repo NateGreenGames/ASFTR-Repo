4b3114cf84de2fb4a544f262b0b3a4e1e34a4f48
MrPancakeMann <samuelj30.sm@gmail.com>
2023-07-07 22:36:18 -0500

    soDish created

---
+++ AShrimpFriedThisRice/Assets/Script/soDish.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soDish : ScriptableObject
+{
+    public string dishName;
+    public Sprite dishImg;
+    public Mesh dishMesh;
+    public ItemSO[] soItem;
+    
+}
