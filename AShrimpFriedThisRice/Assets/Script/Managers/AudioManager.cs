d24a342ed8f4a468d6ebfca4ea15fac8cb15e77a
xAn01x <nguyen.anton13@gmail.com>
2023-07-08 20:03:35 -0500

    Added audio code (not tested)
    Not tested due to no audio tracks/clips

--- AShrimpFriedThisRice/Assets/Script/Managers/AudioManager.cs
+++ AShrimpFriedThisRice/Assets/Script/Managers/AudioManager.cs
@@ -1,18 +1,72 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections.Generic;
+using System.Collections;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] AudioMixer mixer;
+    [SerializeField] AudioSource audioSource;
+
+    public const string MUSIC_KEY = "musicVolume";
+    public const string SFX_KEY = "sfxVolume";
+    public float targetValue = 0;
+    public float durationValue = 5;
+
+
+    // Sound clips
+    [SerializeField] private AudioClip running;
+    [SerializeField] private AudioClip cooking;
+    [SerializeField] private AudioClip clicking;
+
+    // BG Music 
+    [SerializeField] private AudioClip mainMenu;
+    [SerializeField] private AudioClip[] levels;
+
+
+
+    private void Awake()
     {
-        
+        LoadVolume();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        StartBGM();
+    }
+
+    private void LoadVolume()
+    {
+        float musicVolume = PlayerPrefs.GetFloat(MUSIC_KEY, .4f);
+        float sfxVolume = PlayerPrefs.GetFloat(SFX_KEY, .4f);
+    }
+
+
+    public void ButtonSFX()
+    {
+        AudioClip clip = clicking;
+        audioSource.PlayOneShot(clip);
+    }
+
+    IEnumerator SongFadeIn(float _endValue, float _duration) //Lerps AudioSource Volume (NOT MIXER) from targetValue to _duration
+    {
+        float time = 0;
+        float startValue = audioSource.volume;
+
+        while (time < _duration)
+        {
+            audioSource.volume = Mathf.Lerp(startValue, _endValue, time / _duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        audioSource.volume = _endValue;
+    }
+
+    IEnumerator StartBGM()
+    {
+        audioSource.PlayOneShot(mainMenu);
+        StartCoroutine(SongFadeIn(targetValue, durationValue));
+        yield return new WaitForSeconds(mainMenu.length);
+        StartCoroutine(StartBGM());
     }
 }
--- AShrimpFriedThisRice/Assets/Script/Settings.cs
+++ AShrimpFriedThisRice/Assets/Script/Settings.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class Settings : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] AudioMixer mixer;
+    [SerializeField] Slider musicSlider;
+    [SerializeField] Slider sfxSlider;
+
+    public const string MIXER_MUSIC = "MusicVol";
+    public const string MIXER_SFX = "SoundVol";
+
+    private void Awake()
     {
-        
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        musicSlider.value = PlayerPrefs.GetFloat(AudioManager.MUSIC_KEY, .4f);
+        sfxSlider.value = PlayerPrefs.GetFloat(AudioManager.SFX_KEY, .4f);
     }
+
+    public void SetMusicVolume(float _volume)
+    {
+        mixer.SetFloat(MIXER_MUSIC, Mathf.Log10(_volume) * 20);
+    }
+
+    public void SetSFXVolume(float _volume)
+    {
+        mixer.SetFloat(MIXER_SFX, Mathf.Log10(_volume) * 20);
+    }
+
 }
